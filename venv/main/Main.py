8341473094fa2b0c8295b491d7f090feea20ab48
tianxiaolong <tttx0307@163.com>
2019-07-10 23:51:03 +0800

    init project

---
+++ venv/main/Main.py
@@ -0,0 +1,16 @@
+print('100+200:', 100 + 200)
+
+
+a = 100
+if a > 100:
+    print(a)
+else:
+    print(-a)
+
+print("start String")
+
+print("start length:",len('str'))
+
+# list
+list = ['a', 'b', 'c']
+print('list length:{0} index 0:{1}'.format(len(list), list[0]))
