9041e29f3304a04ef8162c11850fafb14dd512be
tianxiaolong <tttx0307@163.com>
2019-07-11 23:47:39 +0800

    加入方法实现默认参数

--- venv/main/method/CreateMethodTest.py
+++ venv/main/method/CreateMethodTest.py
@@ -31,3 +31,32 @@ def testTryMethod(x):
 
 def getName():
     return "田晓龙", '张超'
+
+
+# 默认参数  默认参数必须放到后面 默认参数必须指向不可变当对象 看下面错误例子 eg
+
+def add(a, b=1, c=1):
+    return a + b
+
+
+print(add(1, 2, 3))
+
+
+def eg(list=[]):
+    list.append(1)
+    return list
+
+
+# 这里第二次打印当时候就出错了 因为python在定义函数当就指定好了默认值 即内存当地址 可以使用none来实现
+print(eg())
+print(eg())
+
+
+def eg1(list=None):
+    if list is None:
+        list = []
+    list.append(1)
+    return list
+
+print(eg1())
+print(eg1())
